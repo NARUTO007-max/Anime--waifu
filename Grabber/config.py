b435e5c89e41025c2de8a06af74ca361d95c62ee
NARUTO007-max <nu3762800@gmail.com>
2025-06-01 04:12:38 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -2,13 +2,13 @@ import os
 
 OWNER_ID = 7576729648
 GROUP_ID = "-1002535643821"
-TOKEN = "7646699271:AAEPvPSJsjrJLsr92wWPsoYQ_uQFOsqzW1g"
+TOKEN = "7929416862:AAHZrKaPqiIEmGf6oemjQT1truMPeQCn2-g"
 api_id = 21218274
 api_hash = "3474a18b61897c672d315fb330edb213"
 PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
 SUPPORT_CHAT = "Animeheaven_community"
 UPDATE_CHAT = "Animeheaven_community"
-BOT_USERNAME = "rand_on_sell_bot"
+BOT_USERNAME = "Hinatawaifu_bot"
 CHARA_CHANNEL_ID = -1002659827150
 MONGO_URL = "mongodb+srv://waifu:<yash2005@>@cluster0.ampo8t7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "waifu"
