e846e660e53bb87e5ce9fefd44f1b62db6a97b9f
NARUTO007-max <nu3762800@gmail.com>
2025-06-12 04:34:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from urllib.parse import quote_plus
+
+OWNER_ID = 7576729648
+GROUP_ID = "-1002535643821"
+TOKEN = "7207574948:AAGRV7Te9gcinmy9hLnPvAOiEzcOYgllDns"
+api_id = 21218274
+api_hash = "3474a18b61897c672d315fb330edb213"
+PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
+SUPPORT_CHAT = "Animeheaven_community"
+UPDATE_CHAT = "Animeheaven_community"
+BOT_USERNAME = "Hinatawaifu_bot"
+CHARA_CHANNEL_ID = -1002659827150
+
+# MongoDB credentials — ENCODE them properly
+mongo_user = quote_plus("sufyan532011")
+mongo_pass = quote_plus("2010")
+
+MONGO_URL = f"mongodb+srv://sufyan532011:2010@dbz.28ftn.mongodb.net/?retryWrites=true&w=majority&appName=DBZ"
+DATABASE_NAME = "sufyan532011"
+LOG_CHAT_ID = -1002535643821
